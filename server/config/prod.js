7c63e32c441841e01cec8e909d488de7074e8a0f
jeffreygatbonton <jeff@creationcrate.com>
2020-07-19 10:31:57 -0400

    finished auth flow, set up dev and prod enviros

---
+++ server/config/prod.js
@@ -0,0 +1,6 @@
+module.export = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 module.exports = app => {
   app.get('/', (req, res) => {
     console.log('hey');
-  })
+  });
 
   app.get(
     '/auth/google',
@@ -13,6 +13,11 @@ module.exports = app => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
